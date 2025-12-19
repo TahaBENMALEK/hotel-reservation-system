e988d126d7a43fbff17f47043610467cc56ac893
Taha BENMALEK <benmalektaha.inpt@gmail.com>
2025-12-19 19:24:38 +0100

    docs: add final documentation for tests and exceptions

--- src/main/java/com/hotelreservation/exceptions/RoomNotFoundException.java
+++ src/main/java/com/hotelreservation/exceptions/RoomNotFoundException.java
@@ -1,5 +1,8 @@
 package com.hotelreservation.exceptions;
 
+/**
+ * Thrown when attempting to access a room that doesn't exist.
+ */
 public class RoomNotFoundException extends RuntimeException {
     public RoomNotFoundException(String message) {
         super(message);
